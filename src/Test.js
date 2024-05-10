d0118295cade3c9aec37635c8ae41fb2265c03c5
DanielValik <danilvalik@gmail.com>
2024-05-10 20:31:56 +0200

    Init with context

---
+++ src/Test.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+function SlowComponent() {
+  // If this is too slow on your maching, reduce the `length`
+  const words = Array.from({ length: 10_000 }, () => "WORD");
+  return (
+    <ul>
+      {words.map((word, i) => (
+        <li key={i}>
+          {i}: {word}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Counter({ children }) {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Slow counter?!?</h1>
+      <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+      {children}
+    </div>
+  );
+}
+
+export default function Test() {
+  return (
+    <Counter>
+      <SlowComponent />
+    </Counter>
+  );
+}
